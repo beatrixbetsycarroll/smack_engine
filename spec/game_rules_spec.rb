fddee765a40d30a9f8a708ed3b11388d17fef017
beatrixbetsycarroll <beatrixcarroll@gmail.com>
2015-01-07 16:50:34 -0500

    have one test

---
+++ spec/game_rules_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "GameEngine::GAME_RULES" do
+
+  describe "GameEngine::GAME_RULES" do
+    it "expects the GAME_RULES to be a hash" do
+      expect(GameEngine::GAME_RULES).to be_a Hash
+    end
+  end
+
+end
