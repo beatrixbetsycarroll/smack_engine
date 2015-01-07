9b3e65c37ffb31fb28d7a95021642e74b9b8dbc8
beatrixbetsycarroll <beatrixcarroll@gmail.com>
2015-01-07 14:10:52 -0500

    took out the spec file which is not being used as of yet. now just have a good trial dummy test that works

--- test/spec/factories/deck_cards.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :deck_card_relationship do
-   association :card
-   association :deck
-  end
-end
--- test/spec/factories/time_periods.rb
+++
@@ -1,5 +0,0 @@
-FactoryGirl.define do
-  factory :time_period do
-    name { Faker::Lorem.word }
-  end
-end
